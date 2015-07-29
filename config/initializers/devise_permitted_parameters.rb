879265812b4c63da6f8e6844b5baa44b92684996
swena56 <swena56@gmail.com>
2015-07-29 01:00:07 -0500

    rails_apps_composer: add README files

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -2,14 +2,13 @@ module DevisePermittedParameters
   extend ActiveSupport::Concern
 
   included do
-    before_action :configure_permitted_parameters
+    before_filter :configure_permitted_parameters
   end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :stripeToken
   end
 
 end
